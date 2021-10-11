877041c5a5c77d2f991b11b2b604022ab2c168be
Wayder Loyola <wayderloyola@gmail.com>
2021-10-11 17:45:38 -0300

    :tada: Challenge - Criando uma aplicação do zero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
+
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import Header from '../components/Header';
+
+import { formatDate } from '../utils';
 
 interface Post {
   uid?: string;
@@ -24,13 +36,120 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  // TODO
+  const [nextPage, setNextPage] = useState<string>(postsPagination.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const formattedPosts = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: formatDate(post.first_publication_date),
+    };
+  });
 
-//   // TODO
-// };
+  const [posts, setPosts] = useState<Post[]>(formattedPosts);
+
+  async function handleLoadMorePosts(): Promise<void> {
+    if (nextPage === null) return;
+    const newPostsPagination = await fetch(nextPage).then(response =>
+      response.json()
+    );
+
+    // console.log('newPostsPagination.next_page->', newPostsPagination.next_page);
+
+    setNextPage(newPostsPagination.next_page);
+
+    // console.log('nextPage->', nextPage);
+
+    const newFormattedPosts = newPostsPagination.results.map(post => {
+      return {
+        ...post,
+        first_publication_date: formatDate(post.first_public_date),
+      };
+    });
+
+    setPosts([...posts, ...newFormattedPosts]);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <Header />
+          {posts?.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div className={commonStyles.info}>
+                  <time>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </time>
+                  <span>
+                    <FiUser />
+                    {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+          {nextPage && (
+            <button type="button" onClick={handleLoadMorePosts}>
+              Carregar mais posts
+            </button>
+          )}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  // TODO
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author', 'posts.content'],
+      pageSize: 2,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  // console.log('postsResponse->', postsResponse);
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  // console.log('postsPagination.next_page->', postsPagination.next_page);
+
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+        next_page: postsPagination.next_page,
+      },
+    },
+
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
